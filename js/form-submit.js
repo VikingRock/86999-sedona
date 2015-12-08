b186b61173f69e7d189ba5d608d2a860393164ee
VikingRock <a.zinchuk.a@gmail.com>
2015-12-08 22:51:39 +0300

    form submission script - as 2 functions

--- js/form-submit.js
+++ js/form-submit.js
@@ -1,35 +1,30 @@
 (function() {
 
-  var button = document.querySelector(".feedback__submit .btn");
+  if (!("FormData" in window)) {
+    return;
+  }
 
+  var form = document.querySelector(".feedback");
 
-    if (button.addEventListener) {
-      button.addEventListener("click", submitForm, false);
-    } else if (button.attachEvent) {
-      button.attachEvent('onclick', submitForm);
-    }
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
 
-  function submitForm() {
-
-    if (!("FormData" in window)) {
-      return;
-    }
-
-    var form = document.querySelector(".feedback");
     var data = new FormData(form);
+    request(data, function(response) {
+      console.log(response);
+    });
+  });
 
+  function request(data, fn) {
     var xhr = new XMLHttpRequest();
-    xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + (new Date()).getTime());
-
+    var time = (new Date()).getTime();
+    xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
     xhr.addEventListener("readystatechange", function() {
       if (xhr.readyState == 4) {
-        console.log(xhr.responseText);
+        fn(xhr.responseText);
       }
-
     });
-
     xhr.send(data);
-
   }
 
 })();
