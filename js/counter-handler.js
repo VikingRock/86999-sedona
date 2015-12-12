28622aba38dfa8b2fee7ce82a8ac1e6a7025d44b
VikingRock <a.zinchuk.a@gmail.com>
2015-12-12 19:34:30 +0300

    persons added

--- js/counter-handler.js
+++ js/counter-handler.js
@@ -1,4 +1,7 @@
 (function() {
+  if (!(document.querySelector("#person-template"))) {
+    return;
+  }
 
   // get all + and - buttons and add event handler onclick for them
 
@@ -16,42 +19,63 @@
     var isOutOfLimit = true;
 
     if (this.classList[0].indexOf("left") !=-1) {
-
-      var counterDecVal = parseInt(this.nextElementSibling.childNodes[1].value);     //getting value of the next element > 2nd child
+      var counter = this.nextElementSibling.childNodes[1];
+      counter.value = countPerson();
+      var counterDecVal = parseInt(counter.value);     //getting value of the next element > 2nd child
       if (counterDecVal > 0) {
         this.nextElementSibling.childNodes[1].value = counterDecVal - 1;
-        area.removeChild(area.lastChild);
+        if (counter.id == "person-amount") {
+          removePerson();
+        }
         isOutOfLimit = false;
+      } else {
+        counter.value = countPerson();
       }
     }
 
     if(this.classList[0].indexOf("right") !=-1) {
-
-      var counterIncVal = parseInt(this.previousElementSibling.childNodes[1].value);  //getting value of the prev element > 2nd child
+      var counter = this.previousElementSibling.childNodes[1];
+      counter.value = countPerson();
+      var counterIncVal = parseInt(counter.value);                                    //getting value of the prev element > 2nd child
       var maxVal = parseInt(this.previousElementSibling.childNodes[1].getAttribute("data-max"));
-      if (counterIncVal < maxVal) {
+      if ((counterIncVal < maxVal) && (counterIncVal >= 0)) {
         this.previousElementSibling.childNodes[1].value = counterIncVal + 1;
-        var html = Mustache.render(template, {
-          "number": parseInt(counterIncVal + 1)
-        });
-        var node = document.createElement("div");
-        node.classList.add("person");
-        node.innerHTML = html;
-        area.appendChild(node);
+        if (counter.id == "person-amount") {
+          addPerson(counterIncVal);
+        }
         isOutOfLimit = false;
+      } else {
+        counter.value = countPerson();
       }
     }
 
     if(isOutOfLimit){
-
       var el = this.classList;
       el.add("red-animation");
-
       setTimeout(function() {
         el.remove("red-animation");
       }, 1000);
     }
 
+  }
+
+  function addPerson(counterVal) {
+    var html = Mustache.render(template, {
+      "number": parseInt(counterVal + 1)
+    });
+    var node = document.createElement("div");
+    node.classList.add("person");
+    node.innerHTML = html;
+    area.appendChild(node);
+  }
+
+  function removePerson() {
+    area.removeChild(area.lastChild);
+  }
+
+  function countPerson() {
+    var persons = document.querySelectorAll(".person");
+    return persons.length;
   }
 
 })();
