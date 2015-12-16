9ca37a1f25a4c970c00a1e538ff68d60c24e404b
VikingRock <a.zinchuk.a@gmail.com>
2015-12-16 11:10:08 +0300

    clean task added

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,31 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     connect = require('gulp-connect'),
     rename = require("gulp-rename"),
-    minify = require("gulp-minify-css");
+    minify = require("gulp-minify-css"),
+    uglify = require('gulp-uglify'),
+    imageop = require('gulp-image-optimization'),
+    clean = require('gulp-clean');
+
+var jsLibs = [
+  './bower_components/moment/min/moment-with-locales.min.js',
+  './bower_components/tap/dist/tap.min.js',
+  './bower_components/mustache.js/mustache.min.js',
+  './source/js/*.js'
+]
+
+var imageArr = [
+  './source/img/**/*.png',
+  './source/img/**/*.jpg',
+  './source/img/**/*.jpeg',
+  './source/img/**/*.gif'
+]
+
+//clean build
+gulp.task('clean', function () {
+  return gulp.src('./build/*', {read: false})
+    .pipe(clean());
+});
+
 
 // Web server
 gulp.task('connect', function() {
@@ -28,39 +52,55 @@ gulp.task('html', function () {
 
 // css
 gulp.task('style', function() {
-  return gulp.src('source/less/style.less')
+  return gulp.src('./source/less/style.less')
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer({browsers: 'last 2 versions'})
     ]))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('./build/css'))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('./build/css'))
     .pipe(connect.reload());
 });
 
 //js build
 gulp.task('scripts', function() {
-  return gulp.src(['./js/*.js', '!./js/script.js'])
+  return gulp.src(jsLibs)
     .pipe(concat('script.js'))
-    .pipe(gulp.dest('./js/'))
+    .pipe(gulp.dest('./build/js/'))
+    .pipe(uglify())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('./build/js/'))
     .pipe(connect.reload());
+});
+
+//images
+gulp.task('images', function(cb) {
+  gulp.src(imageArr).pipe(imageop({
+    optimizationLevel: 3,
+    progressive: true,
+    interlaced: true
+  }))
+    .pipe(gulp.dest('./build/img')).on('end', cb).on('error', cb);
 });
 
 // watch
 gulp.task('watch', function() {
-  gulp.watch('less/**/*.less', ['style']);
-  gulp.watch('js/*.js', ['scripts']);
-  gulp.watch(['./*.html'], ['html']);
+  gulp.watch('./source/less/**/*.less', ['style']);
+  gulp.watch('./source/js/*.js', ['scripts']);
+  gulp.watch(['./source/*.html'], ['html']);
 });
 
 // gulp  (собираем проект и запускаем watch)
 gulp.task('default', function() {
   runSequence(
+    'clean',
+    'copy',
     'style',
     'scripts',
+    'images',
     'connect',
     'watch'
   );
